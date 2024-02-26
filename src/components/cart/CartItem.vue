03531104883d4c4f502d969793beee18212c68bb
ArmanGrigorian <armangrigorian.work@gmail.com>
2024-02-26 21:11:50 +0400

    fixed CartItem width
    modified:   src/components/cart/CartItem.vue

--- src/components/cart/CartItem.vue
+++ src/components/cart/CartItem.vue
@@ -9,7 +9,7 @@ const manageCartList = inject('manage-cart-list')
 
 <template>
   <div
-    class="relative flex flex-col justify-center items-start gap-2 bg-customWhite border rounded-xl shadow-md p-3 transition hover:-translate-y-[2px] hover:shadow-xl max-3xs:w-full"
+    class="relative w-[90%]  flex flex-col justify-center items-start gap-2 bg-customWhite border rounded-xl shadow-md p-3 transition hover:-translate-y-[2px] hover:shadow-xl max-3xs:w-full"
   >
     <button
       @click="manageCartList(sneaker)"
